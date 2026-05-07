7a3861aaebb059cfb083b20b348732e1e9d48d6d
ahmedorabi7 <154468740+ahmedorabi7@users.noreply.github.com>
2026-05-07 15:10:07 +0300

    fix db init

--- app.py
+++ app.py
@@ -158,7 +158,8 @@ def api_expiring():
     } for m in members])
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
